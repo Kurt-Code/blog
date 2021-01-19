84a2f260751cfd70877b50cceecf4771c9f295a9
luna <iszychen@gmail.com>
2021-01-19 14:10:45 +0800

    'auto-deploy'

--- deploy.bat
+++ deploy.bat
@@ -1,14 +1,12 @@
 @echo off
 
-echo building...
-call build.bat
 echo building-complete.
 
 git init
 git add -A
 git commit -m 'auto-deploy'
-git remote add origin https://gitee.com/luna_nov/blog.git
-git remote add github https://github.com/czy1024/blog.git
+git remote add origin git@gitee.com:luna_nov/blog.git
+git remote add github git@github.com:czy1024/blog.git
 
 git pull
 git push  origin HEAD:gh-pages
